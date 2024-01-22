fa4ecbb4989166010cabb64037e626b5dd7697fc
pilot99fsa <132755045+pilot99fsa@users.noreply.github.com>
2024-01-22 22:41:56 +0900

    ロジック等のメモの追加

--- custom-hook/src/App.tsx
+++ custom-hook/src/App.tsx
@@ -4,27 +4,29 @@ import UserCard from './components/UserCard';
 import useAllUsers from './hooks/useAllUsers';
 
 export default function App() {
-  // カスタムフックと受け取る値
+  // カスタムフックから受け取る値
   const { getUsers, userProfiles, loading, error } = useAllUsers();
 
+  // ユーザー情報を取得するボタンがクリックされたときの処理
   function onClickFetchUser() {
     getUsers();
   }
 
   return (
     <div className="App">
+      {/* データを取得するボタン */}
       <button onClick={onClickFetchUser}>データ取得</button>
       <br />
-      {/* 三項演算子を用いて、エラーが存在する場合の表示を作っていく */}
+      {/* 三項演算子を用いて、エラーが存在する場合のUIと、ローディング中のUIを作っていく */}
       {error ? (
         <p style={{ color: 'red' }}>データの取得に失敗しました。</p> // エラーが出てしまった結果。22行目のnエンドポイントのURLに末尾に適当な文字を入力してデータ取得してみよう、エラーになる
       ) : loading ? (
         <p>Loading...</p> // エラーが出ずに、ローディングを表す表示
       ) : (
+        // エラーも回避し、ロードも無事に終わると、以下のコンポーネント(ユーザー情報のカード)が表示される
         <>
-          {' '}
           {userProfiles.map((user) => (
-            <UserCard key={user.id} user={user} /> // エラーも回避し、ロードも無事に終わると、カードが表示される
+            <UserCard key={user.id} user={user} />
           ))}
         </>
       )}
--- custom-hook/src/hooks/useAllUsers.ts
+++ custom-hook/src/hooks/useAllUsers.ts
@@ -7,12 +7,14 @@ import { UserProfile } from '../types/userProfiles';
 import axios from 'axios';
 import { User } from '../types/api/user';
 
+// カスタムフックを定義していく
 function useAllUsers() {
+  // ユーザープロファイルの配列とその更新関数を取得するための useState フック
   const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  // ローディングの表示
-  const [loading, setLoading] = useState(false); // エラーのフラグなので、初期値はfalse
+  // APIからデータを取得中かどうかを示すためのローディング状態とその更新関数
+  const [loading, setLoading] = useState(false);
   // エラーがあるかどうかを判定するstate
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(false); // エラーのフラグなので、初期値はfalse
 
   function getUsers() {
     // ローディングは必ず行われるので初期値はtrue
@@ -20,9 +22,10 @@ function useAllUsers() {
     // エラーは初期値はfalseにする
     setError(false);
 
-    axios
+    axios // axiosでAPIからデータを取得
       .get<Array<User>>('https://jsonplaceholder.typicode.com/users')
       .then((res) => {
+        // 取得したデータをmap関数で展開し、userPlofileの状態をuseStateで更新する
         const data = res.data.map((user) => ({
           id: user.id,
           name: `${user.name}${user.username})`,
@@ -32,12 +35,15 @@ function useAllUsers() {
         setUserProfiles(data);
       })
       .catch(() => {
-        setError(true);
+        setError(true); // エラーが発生したらtrueに設定する
       })
       .finally(() => {
-        setLoading(false);
+        setLoading(false); // ローディング状態を終了
       }); // finallyはES2018から使えるようになった文法なので、tsconfig.jsonで使えるように設定る
   }
+  // 呼び出し元(関数の実行元)にユーザー情報取得関数、ユーザープロファイル、ローディング状態、エラー状態を返す
   return { getUsers, userProfiles, loading, error };
 }
+
+// カスタムフックを他のTSファイルでも使えるようにエクスポートする
 export default useAllUsers;
